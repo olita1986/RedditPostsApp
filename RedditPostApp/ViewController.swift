fb643b9aef8fc4a80f7669184f3d5f84c4203d56
Orlando Arzola <orlando.arzola@externos.bci.cl>
2019-11-17 18:09:33 -0300

    Initial Commit

---
+++ RedditPostApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RedditPostApp
+//
+//  Created by Nisum on 11/17/19.
+//  Copyright © 2019 Orlando Arzola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
